8aac1ef2514b13a0d6174cbe732a5cd5fdd60e84
Sahu <801525@cognizant.com>
2020-01-27 19:30:10 +0530

    product service spec changes

--- src/app/products/shared/product.service.spec.ts
+++ src/app/products/shared/product.service.spec.ts
@@ -3,7 +3,10 @@ import {async, getTestBed, TestBed} from '@angular/core/testing';
 import { ProductService } from './product.service';
 import {
   AngularFirestore,
+  AngularFirestoreCollection,
   AngularFirestoreModule,
+  DocumentChangeAction,
+  QueryDocumentSnapshot
 } from '@angular/fire/firestore';
 import {FileService} from '../../files/shared/file.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
@@ -12,14 +15,16 @@ import {Observable, of} from 'rxjs';
 describe('ProductService', () => {
   let angularFirestoreMock: any;
   let fileServiceMock: any;
-  let fsCollection: any;
+  let fsCollectionMock: any;
   let httpMock: HttpTestingController;
   let service: ProductService;
   beforeEach(() => {
     angularFirestoreMock = jasmine.createSpyObj('AngularFirestore', ['collection']);
-    // fsCollection = jasmine.createSpyObj('AngularFirestoreCollection', ['snapshotChanges'])
-    // angularFirestoreMock.collection.and.returnValue(fsCollection);
-    fileServiceMock = jasmine.createSpyObj('FileService', ['getFileUrl', 'upload'])
+    fsCollectionMock = jasmine.createSpyObj('collection', ['snapshotChanges', 'valueChanges']);
+    angularFirestoreMock.collection.and.returnValue(fsCollectionMock);
+    fsCollectionMock.snapshotChanges.and.returnValue(of([]));
+    fileServiceMock = jasmine.createSpyObj('FileService', ['getFileUrl', 'upload']);
+
     TestBed.configureTestingModule({
       imports: [
         AngularFirestoreModule,
@@ -28,11 +33,10 @@ describe('ProductService', () => {
       providers: [
         {provide: AngularFirestore, useValue: angularFirestoreMock},
         {provide: FileService, useValue: fileServiceMock}
-
       ]
     });
-    service = TestBed.get(ProductService);
     httpMock = getTestBed().get(HttpTestingController);
+    service = TestBed.get(ProductService);
   });
 
   it('should be created', () => {
@@ -42,19 +46,18 @@ describe('ProductService', () => {
   describe('getProductsCalls', () => {
     beforeEach(() => {
       service.getProducts();
-    })
-    it('should call collection and snapshot changes on angularfitrestore', () => {
+    });
+
+    it('should call collection 1 time on AngularFirestore service', () => {
       expect(angularFirestoreMock.collection).toHaveBeenCalledTimes(1);
-    })
-    /* it('should call collection 1 time on AngularFirestore service', () => {
-   
     });
 
     it('should call collection with "products" as param', () => {
+      expect(angularFirestoreMock.collection).toHaveBeenCalledWith('products');
     });
 
     it('should call snapshotChanges 1 time on AngularFirestore service', () => {
-      // expect(fsCollectionMock.snapshotChanges).toHaveBeenCalledTimes(1);
-    }); */
+      expect(fsCollectionMock.snapshotChanges).toHaveBeenCalledTimes(1);
+    });
   });
 });
